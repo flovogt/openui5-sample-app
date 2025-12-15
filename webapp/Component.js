665fdfc531a52384e71a56444df794c32bde9a58
Florian Vogt <florian.vogt@sap.com>
2025-12-15 10:28:34 +0100

    feat: Introduce legacy code

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,8 +2,7 @@ sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], (UICo
 	"use strict";
 	return UIComponent.extend("sap.ui.demo.todo.Component", {
 		metadata: {
-			manifest: "json",
-			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
 		}
 	});
 });
