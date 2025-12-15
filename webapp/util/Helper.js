53c4ea839866c08dfed507550338939caa778187
Florian Vogt <florian.vogt@sap.com>
2025-12-15 10:28:34 +0100

    feat: Introduce legacy code

--- webapp/util/Helper.js
+++ webapp/util/Helper.js
@@ -1,9 +1,8 @@
-sap.ui.define(["require"], (require) => {
-	"use strict";
-	return {
-		resolvePath(sPath) {
-			// Relative to application root
-			return require.toUrl("../") + sPath;
-		}
-	};
-});
+jQuery.sap.declare("sap.ui.demo.todo.util.Helper");
+
+sap.ui.demo.todo.util.Helper = {
+	resolvePath(sPath) {
+		// Relative to application root
+		return sap.ui.require.toUrl("sap/ui/demo/todo/" + sPath);
+	}
+};
